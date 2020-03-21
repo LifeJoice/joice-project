fe039e76a1cc18bcf5b7d27256b3d715beccce16
chenchuanzi <chenchuanzi@polycn.com>
2020-03-21 21:10:20 +0800

    init

---
+++ src/main/java/org/misi/jyc/JycApplication.java
@@ -0,0 +1,13 @@
+package org.misi.jyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JycApplication.class, args);
+    }
+
+}
